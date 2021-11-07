550c2abea3a607b94f86137bc72455ad240b5732
jedinrk <jedi.nrk@gmail.com>
2021-11-07 23:08:20 +0400

    Added tests to add liquidity and swap token

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,33 +22,28 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    },
-    hardhat: {
-      forking: {
-        url: "https://speedy-nodes-nyc.moralis.io/c824a9937ea73ef02c0c195f/bsc/mainnet",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
       }
+    }
+  },
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0,
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    forked: {
+      url: "http://127.0.0.1:8545/",
     },
-    testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      chainId: 97,
-      gasPrice: 20000000000,
-      accounts: {mnemonic: process.env.MNEMONIC}
-    },
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
+    binance: {
+      url: process.env.BINANCE_RPC,
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: {mnemonic: process.env.MNEMONIC}
-    }
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
--- test/brainiac.js
+++ test/brainiac.js
@@ -1,28 +1,47 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+
 describe("Brainiac contract", function () {
-  let Brainiac, brainiac, owner, addr1, addr2;
+  let Brainiac, brainiac, owner, user0, user1, user2;
   const buyLimit = 100;
   const feeRewardPct = 750;
   let feeRewardAddress;
-  const router = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
+  const routerAddr = process.env.ROUTER_ADDRESS;
+  const factoryAddr = process.env.FACTORY_ADDRESS;
+  let routerInstance, factoryInstance, wbnbInstance;
+  const MAX_APPROVE_AMOUNT = ethers.BigNumber.from(
+    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+  );
 
-  beforeEach(async () => {
-    [owner, addr1, addr2] = await hre.ethers.getSigners();
+  before(async () => {
+    routerInstance = await ethers.getContractAt(
+      "IUniswapV2Router02",
+      routerAddr
+    );
 
-    feeRewardAddress = owner.address;
+    factoryInstance = await ethers.getContractAt(
+      "IUniswapV2Factory",
+      factoryAddr
+    );
 
+    wbnbInstance = await ethers.getContractAt("IERC20", WBNB);
+    [owner, user0, user1, user2] = await hre.ethers.getSigners();
+
+    feeRewardAddress = user2.address;
+  });
+
+  it("Should have deployed Brainiac", async function () {
     Brainiac = await ethers.getContractFactory("Brainiac");
-    brainiac = await Brainiac.deploy();
-    await brainiac.deployed();
-
-    const initializer = await brainiac.initialize(
+    brainiac = await Brainiac.deploy(
       buyLimit,
       feeRewardPct,
       feeRewardAddress,
-      router
+      routerAddr
     );
+
+    expect(await brainiac.deployed());
   });
 
   it("should assign the total supply of tokens to the owner", async function () {
@@ -39,16 +58,94 @@ describe("Brainiac contract", function () {
   });
 
   it("should be able to transfer from an account to account without fees", async function () {
-    const transferAmount = 50;
+    const transferAmount = ethers.utils.parseEther("100000");
     const totalSupply = await brainiac.totalSupply();
 
-    await brainiac.transfer(addr1.address, transferAmount);
+    await brainiac.transfer(user0.address, transferAmount);
 
     const owner_balance = await brainiac.balanceOf(owner.address);
-    const addr1_balance = await brainiac.balanceOf(addr1.address);
-    
-    expect(addr1_balance.toString()).to.equal(transferAmount.toString());
+    const user0_balance = await brainiac.balanceOf(user0.address);
+
+    expect(user0_balance).to.equal(transferAmount);
 
     //expect(owner_balance.toString()).to.equal((totalSupply-50).toString());
   });
+
+  it("owner should create pair and add liquidity", async () => {
+    await brainiac.approve(routerAddr, MAX_APPROVE_AMOUNT);
+    await wbnbInstance.approve(routerAddr, MAX_APPROVE_AMOUNT);
+
+    const allowance = await brainiac.allowance(owner.address, routerAddr);
+
+    const amountTokenDesired = ethers.utils.parseUnits("10000", "ether");
+    const amountTokenMin = amountTokenDesired;
+    const amountETHMin = ethers.utils.parseUnits("100", "ether");
+
+    await routerInstance.addLiquidityETH(
+      brainiac.address,
+      amountTokenDesired,
+      amountTokenMin,
+      amountETHMin,
+      owner.address,
+      new Date(new Date().getTime() + 10 * 60000).getTime(),
+      { value: ethers.utils.parseEther("1000") }
+    );
+
+    let pairAddress = await factoryInstance.getPair(brainiac.address, WBNB);
+    console.log("pairAddress", pairAddress);
+  });
+
+  it("Should ensure the fees collected after adding liquidity by owner is zero", async () => {
+    expect(await brainiac.balanceOf(feeRewardAddress)).to.equal(0);
+  });
+
+  it("Should be able to swap a BNB for the Brainiac token", async () => {
+    const tokenAmountIn = ethers.utils.parseUnits("0.01", "ether");
+
+    const pair = await factoryInstance.getPair(brainiac.address, WBNB);
+    console.log("pair ", pair);
+
+    let tokenBalanceBeforeSwap = await brainiac.balanceOf(user1.address);
+    console.log("tokenBalanceBeforeSwap", tokenBalanceBeforeSwap.toString());
+
+    const amounts = await routerInstance.getAmountsOut(tokenAmountIn, [
+      brainiac.address,
+      WBNB,
+    ]);
+
+    const amountOut = amounts[1];
+    const expectedBrainToken = amountOut;
+    console.log("amountOut ", expectedBrainToken.toString());
+
+    await routerInstance
+      .connect(user1)
+      .swapETHForExactTokens(
+        amountOut,
+        [WBNB, brainiac.address],
+        user1.address,
+        new Date(new Date().getTime() + 10 * 60000).getTime(),
+        { value: tokenAmountIn }
+      );
+
+    let brainToken = await brainiac.connect(user1).balanceOf(user1.address);
+    const actualBrainToken = brainToken.toString();
+
+    expect(expectedBrainToken).to.equal(actualBrainToken);
+  });
+
+  it("Should ensure the fee has been collected after the swap", async () => {
+    let brainiacFeeBalance = await brainiac
+      .connect(user2)
+      .balanceOf(feeRewardAddress);
+    console.log("brainiacFeeBalance ", brainiacFeeBalance.toString());
+  });
 });
+
+async function seedToken(tokenInstance, account) {
+  await tokenInstance.connect(account);
+  await tokenInstance.deposit({ value: ethers.utils.parseEther("500") });
+
+  const symbol = await tokenInstance.symbol();
+  const tokenBalance = await tokenInstance.balanceOf(account.address);
+  console.log(symbol + " Token balance of owner: ", tokenBalance.toString());
+}
